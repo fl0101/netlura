ab5824988eb4a7b5bd69a4bf6628dd1bf31ebfd4
Flaviane Nascimento <38900126+fl0101@users.noreply.github.com>
2026-03-27 21:30:17 -0300

    Update media links and items in data.js

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -3,39 +3,39 @@ export const categories = [
         title: "Épicos",
         items: [
             {
-                img: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=bLvqoHBptjg"
+                img: "https://m.media-amazon.com/images/S/pv-target-images/51d8c0540f392dd3e8ccb1c6b82527be71422a7ed771fdedb61164f19c0858b5.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=pjyih9cmfKk"
             },
             {
-                img: "https://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=cXg62-t8BWs"
+                img: "https://m.media-amazon.com/images/S/pv-target-images/0bae69962e2601aa806b1148a92472f2f0c430c00de7f5a4124314946b50192d.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=nqmPqJA-LeI"
             },
             {
-                img: "https://i.ytimg.com/vi/OQgySPQ5M3Y/maxresdefault.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=zckJCxYxn1g"
+                img: "https://m.media-amazon.com/images/S/pv-target-images/f4fec0ddd7ae368cd76808590229adb0e75bca6ae67cfe0fbb373d17e83b5505.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=p_I4dDg57t0"
             },
             {
-                img: "https://ingresso-a.akamaihd.net/img/cinema/cartaz/14413-destaque.jpg", progress: 0,
-                youtube: "https://www.youtube.com/watch?v=a06zxOyQrAs"
+                img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhZ4CjPPcaO5FU47D0exl2jUyMvhxyg8QGXnNBe29EjflktdJXo2AVn7Rtpytq47VM2BeBsphgGmXWjGd1inPkrI0E6SMGqZAEpb_rWG1P2mNBtBdH9WPe3j0xwXEFwe4UNHK4AlsoNOmQ/w1200-h630-p-k-no-nu/1946122-concours-jack-et-la-mecanique-du-coeur-votre-enfant-dans-un-moment-d-amour.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=4o-4j0o-bqE"
             },
         ]
     },
     {
         title: "Séries",
         items: [
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1505686994434-e3cc5abf1330?q=80&w=600&auto=format&fit=crop", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://plus.unsplash.com/premium_photo-1724835454003-dbcc05637cf7?q=80&w=812&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=4ynlhNdwAVw" },
+            { img: "https://images.unsplash.com/photo-1631582053308-40f482e7ace5?q=80&w=1031&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", top10: true, youtube: "https://www.youtube.com/watch?v=cyp7d-jEvNA" },
+            { img: "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/6237f381-2ab7-41ca-8e73-e177033de02f/bf2f7417-b348-11ef-9fed-0affd17b6387?host=wbd-images.prod-vod.h264.io&partner=beamcom", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=-XygqC4rpAs" },
+            { img: "https://static.wikia.nocookie.net/fiction-battlefield/images/8/8a/Gumball-teoria.jpg/revision/latest?cb=20181203100321&path-prefix=pt-br", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=IWPe576TQOI" },
         ]
     },
     {
         title: "Para maratonar",
         items: [
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?q=80&w=600&auto=format&fit=crop", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/198a6232638412030a51f24d13186658893eddd3182b1055b37be61dd5d653e3.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=tCed6P6qlfg&list=PLL7-MKargbpcM7PsoMKcGMo3M8TjN6L7P&index=9" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/48058ee18cf7663a830ef31c8994d8d980fa9c989b0ef85cb13ab69e2c9e7e1f.jpg", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=MaKF-pDn2wc&list=LL&index=26" },
+            { img: "https://s2-techtudo.glbimg.com/7gudZIuRmFvHYK8mUE3PPVIoK-s=/0x0:620x320/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2021/m/S/A850ieROGGpDNXNqxPXg/2011-11-18-wow-logo.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=4MKtq_9n7RI&list=LL&index=9" },
+            { img: "https://m.media-amazon.com/images/M/MV5BYThlMTQ3Y2YtZDA4Mi00OGFhLTkxMWItNjk5YTE2MTNmNDgzXkEyXkFqcGc@._V1_.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=EdsC5J-FzJ8" },
         ]
     }
 ];
